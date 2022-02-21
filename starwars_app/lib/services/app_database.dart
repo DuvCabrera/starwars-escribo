ff0645520d2e56c1fee2b1f0f0616e8171d1675f
Duv <eduardo@escribo.com>
2022-02-21 18:28:22 -0300

    start refactory

--- starwars_app/lib/services/app_database.dart
+++ starwars_app/lib/services/app_database.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:starwars_app/services/avatar_dao.dart';
 import 'package:starwars_app/services/star_war_dao.dart';
 
-Future<Database>() async {
+Future<Database> createDataBase() async {
   final String path = join(await getDatabasesPath(), 'starwars.db');
   return openDatabase(path, onCreate: (db, version) {
     db.execute(StarWarsDao.tableSql);
--- starwars_app/lib/services/star_war_dao.dart
+++ starwars_app/lib/services/star_war_dao.dart
@@ -3,7 +3,7 @@ import 'app_database.dart';
 import 'package:starwars_app/interfaces/local_storage_interface.dart';
 import 'package:starwars_app/models/favorite.dart';
 
-class StarWarsDao implements ILocalStorage{
+class StarWarsDao implements ILocalStorage {
   static const String tableSql = 'CREATE TABLE $_tableName('
       '$_id INTEGER PRIMARY KEY AUTOINCREMENT,'
       '$_json TEXT,'
@@ -13,7 +13,6 @@ class StarWarsDao implements ILocalStorage{
   static const String _id = 'id';
   static const String _json = 'json';
   static const String _type = 'type';
-
 
   @override
   Future delete(int key) async {
@@ -29,21 +28,20 @@ class StarWarsDao implements ILocalStorage{
   }
 
   @override
-  Future put(String json, type) async{
+  Future put(String json, type) async {
     final Database db = await createDataBase();
-    final Favorite favorite = Favorite(json,type);
+    final Favorite favorite = Favorite(json, type);
     return db.insert(_tableName, favorite.toMap());
   }
 
   @override
   Future update(String key, id) {
-
     throw UnimplementedError();
   }
 
   List<Favorite> toList(List<Map<String, dynamic>> result) {
     final List<Favorite> favorites = [];
-    for (Map<String, dynamic> map in result){
+    for (Map<String, dynamic> map in result) {
       favorites.add(Favorite(map[_json], map[_type], id: map[_id]));
     }
     return favorites;
